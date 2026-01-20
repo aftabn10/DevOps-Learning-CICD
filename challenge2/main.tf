e7d767054a627921379e03c0ee2dd842dbe31535
aftabn10 <aftab@mediadezines.co.uk>
2026-01-20 23:27:46 +0000

    My second CICD pipeline

--- challenge2/main.tf
+++ challenge2/main.tf
@@ -1,13 +1,11 @@
-provider "aws" {
-  region = "eu-west-2"
+provider "aws"{
+region="eu-west-2"
 }
-
 # setup an S3 bucket
-resource "aws_s3_bucket" "tf_linting" {
-  bucket = "challenge2-tf-linting"
-
-  tags = {
-    Name        = "challenge2-tf-linting"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "tf_linting"{
+bucket="challenge2-tf-linting"
+tags={
+Name="challenge2-tf-linting"
+Environment="Dev"
+}
 }
